72a0fd8831fa2117b41169362d5785d4bd9ae0cd
unknown <you@example.com>
2025-11-13 09:31:47 +0200

    group

--- group_me.py
+++ group_me.py
@@ -1,16 +1,24 @@
-from typing import Callable
+from typing import List, Dict, Callable
+from typing import TypeVar
 
 
-def group(f: Callable, x: list) -> dict:
-    """Groups list items by f(item) in a dictionary
+T = TypeVar('T')
+S = TypeVar('S')
+
+
+def group(f: Callable[[S], T], group_items: List[S]) -> Dict[T, List[S]]:
+    """Groups group_items by f(item) in a dictionary
     :param f: function with return value
-    :x: list of items to be grouped
+    :group_items: list of items to be grouped
+    Returns dictionary where each key is a value of f and is mapped to a list of
+    items from group_items whose f result is that value
     """
-    group_dict: dict = {}
-    for item in x:
-        val = f(item)
-        if val in group_dict:
-            group_dict[val].append(item)
-        else:
-            group_dict[val] = [item]
-    return group_dict
+    vals = {f(item) for item in group_items}
+    return {val: [item for item in group_items if f(item) == val] for val in vals}
+
+
+def mod_3(num: int) -> int:
+    return num % 3
+
+
+print(group(mod_3, [1, 3, 4, 5, 7, 9]))
